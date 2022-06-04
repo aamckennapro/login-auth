7e426f27e3fb403d8d7270ae1048437eddcb1a89
aamckennapro <aamckennapro@gmail.com>
2022-06-04 13:54:24 -0400

    feat: new salt string generation

--- authentication.py
+++ authentication.py
@@ -5,3 +5,8 @@ def generate_token():
 	alphabet = string.ascii_letters + string.digits
 	token = ''.join(secrets.choice(alphabet) for i in range(6))
 	return token
+
+def grind_salt():
+	alphabet = string.ascii_letters + string.fixed_digits
+	salt_str = ''.join(secrets.choice(alphabet) for i in range(16))
+	return salt_str
